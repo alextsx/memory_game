730dfeeeaa6fcafce369077e7f7715c9b9ced7d2
alex <76518513+alextsx@users.noreply.github.com>
2025-06-01 18:14:27 +0200

    style: add cursor pointers and disable highlighting

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export const Logo = () => {
-  return <Image src="/assets/img/logo.svg" width={215} height={119} alt="Memo mission logo" />;
+  return (
+    <Image
+      src="/assets/img/logo.svg"
+      width={215}
+      height={119}
+      alt="Memo mission logo, starts the game"
+    />
+  );
 };

8c9a8071001886bffd83ebb4f297597f8cf6507b
alex <76518513+alextsx@users.noreply.github.com>
2025-06-01 18:20:28 +0200

    feat: add animation to settings modal

--- src/components/header/menu/settings/SettingsModal.tsx
+++ src/components/header/menu/settings/SettingsModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Portal } from '@/hocs/Portal';
 import { SettingsModalBody } from './SettingsModalBody';
 import { SettingsModalHeader } from './SettingsModalHeader';
@@ -23,17 +24,33 @@ export const SettingsModal = () => {
        im using portal here to render it outside main app dom tree
        its useful for stuff like screen readers and parent z index wont affect this
       */}
-      {isOpen && (
-        <Portal selector="modal-root">
-          <div className="modal">
-            <div className="modal-overlay" onClick={handleClick} />
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-              <SettingsModalHeader onClose={handleClick} />
-              <SettingsModalBody onSave={handleSave} />
+      <AnimatePresence>
+        {isOpen && (
+          <Portal selector="modal-root">
+            <div className="modal">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2, ease: 'easeInOut' }}
+                className="modal-overlay"
+                onClick={handleClick}
+              />
+              <motion.div
+                initial={{ y: '-100vh' }}
+                animate={{ y: 0 }}
+                exit={{ y: '-100vh' }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                className="modal-content"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <SettingsModalHeader onClose={handleClick} />
+                <SettingsModalBody onSave={handleSave} />
+              </motion.div>
             </div>
-          </div>
-        </Portal>
-      )}
+          </Portal>
+        )}
+      </AnimatePresence>
     </>
   );
 };

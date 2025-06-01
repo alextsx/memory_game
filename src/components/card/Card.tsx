b4ba7ce210c8bdccbb70f516cdb1ae6c13740797
alex <76518513+alextsx@users.noreply.github.com>
2025-06-01 18:15:09 +0200

    feat: add card hover animation via framer motion

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { CardBack } from './CardBack';
 import { CardFront } from './CardFront';
 
@@ -13,19 +14,17 @@ export const Card = ({ animalId }: CardProps) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
-  useEffect(() => {
-    setIsHovered(Math.random() < 0.5);
-  }, []);
-
-  useEffect(() => {
-    if (animalId > 10) {
-      setIsFlipped(Math.random() < 0.5);
-    }
-  }, [animalId]);
-
   return (
-    <div className="card">
+    <motion.div
+      className="card"
+      onClick={() => setIsFlipped(!isFlipped)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      initial={false}
+      /* shadow on hover */
+      animate={{ boxShadow: isHovered ? '0 1px 4px rgba(0, 0, 0, 0.2)' : 'none' }}
+    >
       {isFlipped ? <CardFront animalId={animalId} /> : <CardBack isHovered={isHovered} />}
-    </div>
+    </motion.div>
   );
 };
--- src/components/card/CardBack.tsx
+++ src/components/card/CardBack.tsx
@@ -1,20 +1,33 @@
+'use client';
+
 import Image from 'next/image';
-
-const NonHoveredCardBackSvg = () => {
-  return <Image src="/assets/img/back.svg" width={81} height={130} alt="Non hovered card's back" />;
-};
-
-const HoveredCardBackSvg = () => {
-  return (
-    <Image
-      src="/assets/img/back_hovered.svg"
-      width={81}
-      height={130}
-      alt="Non hovered card's back"
-    />
-  );
-};
+import { motion } from 'framer-motion';
 
 export const CardBack = ({ isHovered }: { isHovered: boolean }) => {
-  return <>{isHovered ? <HoveredCardBackSvg /> : <NonHoveredCardBackSvg />}</>;
+  return (
+    <motion.div
+      className="card-back"
+      initial={false}
+      animate={{ scale: isHovered ? 1.075 : 1 }}
+      transition={{ duration: 0.2, ease: 'easeInOut' }}
+    >
+      <Image
+        src="/assets/img/back.svg"
+        width={81}
+        height={130}
+        alt="Non hovered card's back"
+        className="absolute top-0 left-0 transition-opacity duration-200"
+        style={{ opacity: isHovered ? 0 : 1 }}
+      />
+
+      <Image
+        src="/assets/img/back_hovered.svg"
+        width={81}
+        height={130}
+        alt="Hovered card's back"
+        className="absolute top-0 left-0 transition-opacity duration-200"
+        style={{ opacity: isHovered ? 1 : 0 }}
+      />
+    </motion.div>
+  );
 };
